5422e4451fb06ceb1d5a90410772a72f1e7c090b
letici0costa <109427133+letici0costa@users.noreply.github.com>
2023-07-10 12:34:04 -0300

    Add files via upload

---
+++ Prova Prática/Questão 01/index.js
@@ -0,0 +1,11 @@
+ const http = require('http')
+ console.log(http)
+ const PORT = 3000
+
+ const server = http.createServer((req, res)=>{
+    res(write)
+
+    res.setHeader('Content-Type', 'application/json');
+ })
+
+
